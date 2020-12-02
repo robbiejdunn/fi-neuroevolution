13794682b59ad40a3fcd4dbe838c17fb3fc5f448
robbiejdunn <robbiejdunn94@gmail.com>
2020-12-02 14:26:34 +0000

    test setup.cfg

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ import re
 
 from setuptools import setup
 
+
 with open("src/fiagent/__init__.py", encoding="utf8") as f:
     version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
+
 setup(
-    name="fiagent",
+    name="FIAgent",
     version=version,
 )
 
