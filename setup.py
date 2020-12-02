e9eced546eb50e17644ec574d4c1870ff81e3708
robbiejdunn <robbiejdunn94@gmail.com>
2020-12-02 16:05:31 +0000

    javagateway setup

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
+import os
 import re
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
-with open("src/fiagent/__init__.py", encoding="utf8") as f:
+here_path = os.path.abspath(os.path.dirname(__file__))
+
+
+with open(os.path.join(here_path, "fiagent/__init__.py"), encoding="utf8") as f:
     version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
 
 setup(
-    name="FIAgent",
+    name="fiagent",
     version=version,
+    packages=find_packages()
 )
 
